686e1d376a1171bc5889c6fbbe4d5b48158ccf35
Diego Staniescki <Diego.stk@live.com>
2023-02-03 19:05:00 -0300

    Atualizacao de pagina sobre e header com logos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
+
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
+// @ts-ignore
+import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -7,8 +10,9 @@ import { MainComponent } from './components/main/main.component';
 import { ProfessoraComponent } from './components/professora/professora.component';
 import {AppRoutingModule} from "./app.routing.module";
 import { FooterPageComponent } from "./components/footer/footer-page.component"
-import {RouterModule} from "@angular/router";
+import {RouterModule} from '@angular/router'
 
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
